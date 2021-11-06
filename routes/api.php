04e52ed2d64515cd8f42f604d08498daa4fcc68f
Chan Ei Hmwe <chaneihmweit@gmail.com>
2021-11-06 09:30:55 +0630

    Rm Auth API

--- routes/api.php
+++ routes/api.php
@@ -30,22 +30,22 @@ Route::group(['prefix' => 'v1'], function () {
 
 });   
 
-Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify']], function () {
+// Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify']], function () {
     Route::get('logout','Api\UserController@logout');
     Route::get('user','Api\UserController@user');
-});
+// });
 
 //Creator / User
-Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify', 'role:admin|creator|user']], function () {
+// Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify', 'role:admin|creator|user']], function () {
     Route::resource('category','Api\CategoryController');
-});
+// });
 
 //Creator
-Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify', 'role:creator|user']], function () {
+// Route::group(['prefix' => 'v1', 'middleware' => ['jwt.verify', 'role:creator|user']], function () {
     Route::resource('subscription-plan','Api\SubscriptionPlanController');
     Route::resource('content','Api\ContentController');
     Route::resource('like','Api\LikeController');
     Route::resource('comment','Api\CommentController');
     Route::resource('poll','Api\PollController');
     Route::resource('subscription','Api\SubscriptionController');
-});
+// });
